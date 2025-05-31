3e98caccb735b5f742f9d7ee4190a9db9eb4cce5
Petar Petrov <ppetrov@pbyte.xyz>
2025-05-31 19:17:46 +0300

    Removed tailwind tools plugin.

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -6,15 +6,4 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ft = { 'html', 'templ' },
   },
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    event = "VeryLazy",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "neovim/nvim-lspconfig",
-    },
-    opts = {}
-  }
 }
