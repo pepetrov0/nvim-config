b02f9d7c9d4bd03c56e37ce672ec23a09c01794e
Petar Petrov <ppetrov@pbyte.xyz>
2025-06-01 17:40:34 +0300

    Changed completion engine over to blink instead of nvim-cmp.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,36 +1,10 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = "LspAttach",
-    dependencies = {
-      'onsails/lspkind-nvim',
-      'hrsh7th/cmp-nvim-lsp',
-      'neovim/nvim-lspconfig',
-    },
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require('lspkind')
-
-      cmp.setup({
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text'
-          })
-        },
-        mapping = {
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<Esc>'] = cmp.mapping.abort(),
-          ['<PageUp>'] = cmp.mapping.scroll_docs(-5),
-          ['<PageDown>'] = cmp.mapping.scroll_docs(5),
-          ['<Up>'] = cmp.mapping.select_prev_item(),
-          ['<Down>'] = cmp.mapping.select_next_item(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        },
-        sources = cmp.config.sources(
-          { { name = 'nvim_lsp' } },
-          { { name = "buffer" } }
-        )
-      })
-    end
-  },
+  "saghen/blink.cmp",
+  version = "1.*",
+  opts = {
+    keymap = { preset = 'enter' },
+    completion = {
+      documentation = { auto_show = true }
+    }
+  }
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ return {
     opts = {
       handlers = {
         function(server_name)
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          local capabilities = require("blink.cmp").get_lsp_capabilities()
           require("lspconfig")[server_name].setup({
             capabilities = capabilities,
           })
