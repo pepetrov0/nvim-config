bc28337ce7026b56d827de7a103153dae9a929a2
Petar Petrov <ppetrov@pbyte.xyz>
2025-06-02 17:21:11 +0300

    Changed treesitter to load on bufreadpre.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  event = "VeryLazy",
+  event = "BufReadPre",
   config = function()
     require('nvim-treesitter.configs').setup({
       highlight = { enable = true },
