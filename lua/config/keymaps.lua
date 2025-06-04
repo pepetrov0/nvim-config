cdbccb46c6ae9be204454f537cdd5edecd04e922
Petar Petrov <ppetrov@pbyte.xyz>
2025-06-04 19:45:08 +0300

    Added keymap for lazy.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "<leader>x", "<cmd>bd<cr>", { desc = "Close current buffer" })
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
