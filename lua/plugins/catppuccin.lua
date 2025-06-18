c54d9861e18f03194d2a85e12f6e5ac037039e61
Petar Petrov <petar.petrov@dss.bg>
2025-06-18 11:42:57 +0300

    Changed lualine to generate the theme based on catppuccin.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,5 @@
-return { 
-	{
-		"catppuccin/nvim", 
-		name = "catppuccin", 
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
 }
